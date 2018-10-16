c1264d321f0c6f76e3c5aba0e4dd4f230c2e4cf4
Syed Faraaz Ahmad <faraaz98@live.com>
2018-10-16 19:23:59 +0530

    add code to select stylesheet engine

--- create.py
+++ create.py
@@ -1,6 +1,7 @@
 # get app name
 def get_app_name():
     app_name = input("Enter app name: ")
+    return app_name
 
 # get preferred view engine
 def get_view_engine():
@@ -15,20 +16,24 @@ def get_view_engine():
         "none"
     ]
     
-    # show all available view engines
     print("Available view engines: ")
+    # show all available view engines
     i = 1
     for eng in view_engine_list:
         print("{}. {}    ".format(i, eng), end = '')
         i += 1
     
-    choice = int(input("\nChoose view engine (1 - 8): "))
-    if choice < 1 and choice > 8:
-        print("Enter a valid number")
-        get_view_engine()
+    # get choice number
+    choice = None
+    while True:
+        choice = int(input("\nChoose view engine (1 - {}): ".format(view_engine_list.__len__())))
+        if choice > 0 and choice < view_engine_list.__len__():
+            break
+        else:
+            print("Invalid input.")
     
+    # select engine string
     engine_string = None
-
     if choice is 8:
         engine_string = "--no-view"
     else:
@@ -37,7 +42,35 @@ def get_view_engine():
     
     return engine_string
 
-# generate `express ..` arguments
+
+def get_stylesheet_engine():
+    stylesheet_engine_list = [
+        "sass",
+        "less",
+        "stylus",
+        "compass"
+    ]
+
+    print("Available stylesheet engines: ")
+    # show all available stylesheet engines
+    i = 1
+    for eng in stylesheet_engine_list:
+        print("{}. {}    ".format(i, eng), end = '')
+        i += 1
+    
+    # get choice number
+    choice = None
+    while True:
+        choice = int(input("\nChoose stylesheet engine (1 - {}): ".format(stylesheet_engine_list.__len__())))
+        if choice > 0 and choice < stylesheet_engine_list.__len__():
+            break
+        else:
+            print("Invalid input.")
+    
+    # generate stylesheet string
+    stylesheet_string = "--css={}".format(stylesheet_engine_list[choice - 1])
+
+    return stylesheet_string
 
 # run `express ..` with arguments
-get_view_engine()
+print("express {} {} {} --git".format(get_view_engine(), get_stylesheet_engine(), get_app_name()))
