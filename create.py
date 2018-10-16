aa220d7ac07af281038d2b36e206da7e60ea9a60
Syed Faraaz Ahmad <faraaz98@live.com>
2018-10-16 18:44:23 +0530

    add code to select view engine

--- create.py
+++ create.py
@@ -1,7 +1,43 @@
 # get app name
+def get_app_name():
+    app_name = input("Enter app name: ")
 
 # get preferred view engine
+def get_view_engine():
+    view_engine_list = [
+        "ejs",
+        "hbs",
+        "hjs",
+        "jade",
+        "pug",
+        "twig",
+        "vash",
+        "none"
+    ]
+    
+    # show all available view engines
+    print("Available view engines: ")
+    i = 1
+    for eng in view_engine_list:
+        print("{}. {}    ".format(i, eng), end = '')
+        i += 1
+    
+    choice = int(input("\nChoose view engine (1 - 8): "))
+    if choice < 1 and choice > 8:
+        print("Enter a valid number")
+        get_view_engine()
+    
+    engine_string = None
+
+    if choice is 8:
+        engine_string = "--no-view"
+    else:
+        view_engine = view_engine_list[choice - 1]
+        engine_string = "--view={}".format(view_engine)
+    
+    return engine_string
 
 # generate `express ..` arguments
 
 # run `express ..` with arguments
+get_view_engine()
